bcd5b48d7a42afd59dc6973b76576c628ba61d57
Bela Stoyan <bela.stoyan@gmail.com>
2023-09-28 20:15:42 +0200

    Remove redundant layer

--- py-adder/src/lib.rs
+++ py-adder/src/lib.rs
@@ -1,9 +1,9 @@
 use pyo3::{pyfunction, wrap_pyfunction, Python, types::PyModule, PyResult, pymodule};
-use some_other_crate::wrapped_add;
+use backend::add;
 
 #[pyfunction]
 pub fn py_add(left: usize, right: usize) -> usize {
-    wrapped_add(left, right)
+    add(left, right)
 }
 
 #[pymodule]
--- some_other_crate/src/lib.rs
+++
@@ -1,5 +0,0 @@
-use backend::add;
-
-pub fn wrapped_add(left: usize, right: usize) -> usize {
-    add(left, right)
-}
